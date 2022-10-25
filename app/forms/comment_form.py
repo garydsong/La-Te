c9298e093e718e09642f6ad979af3c1c039c1162
garydsong <ceouls@gmail.com>
2022-10-25 10:04:08 -0700

    comment form

---
+++ app/forms/comment_form.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, ValidationError,
+
+from app.models import Comment
+
+def valid_comment(form, field):
+  comment = field.data
+  if len(comment) < 1 or len(comment) > 3000:
+    raise ValidationError("Comment must be between 1 and 1000 characters")
+
+class CommentForm(FlaskForm):
+  comment = StringField('Comment', validators=[DataRequired(), valid_comment])
