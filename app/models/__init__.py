798d85302e26e9aa72b2a243bbf11a10d78d5169
garydsong <ceouls@gmail.com>
2022-10-25 08:50:18 -0700

    follower model and latte model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,7 @@
 from .db import db
 from .user import User
+from .comment import Comment
+from .follower import Follower
+from .latte import Latte
+from .post import Post
+from .reaction import Reaction
---
+++ app/models/follower.py
@@ -0,0 +1,15 @@
+from .db import db
+
+class Follower(db.Model):
+    __tablename__ = 'followers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    follower_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    followed_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'follower_id': self.follower_id,
+            'followed_id': self.followed_id
+        }
