6b13cc0ea9e09206b6dc9d1339ba8eb9c0c74c45
garydsong <ceouls@gmail.com>
2022-10-25 08:40:14 -0700

    comment model

---
+++ app/models/comment.py
@@ -0,0 +1,30 @@
+from .db import db
+from sqlalchemy.sql import func
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String(1000), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True),
+                           server_default=func.current_timestamp())
+    updated_at = db.Column(db.DateTime(timezone=True),
+                           onupdate=func.current_timestamp())
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable=False)
+    reaction_id = db.Column(db.Integer, db.ForeignKey("reactions.id"), nullable=False)
+
+    users = db.relationship("User", back_populates="comments")
+    posts = db.relationship("Post", back_populates="comments")
+    reactions = db.relationship("Reaction", back_populates="comments")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'comment': self.comment,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at,
+            'user_id': self.user_id,
+            'post_id': self.post_id,
+            'reaction_id': self.reaction_id
+        }
