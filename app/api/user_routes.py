c83e2c9ccceafff779e96d5c2bd28894f6bdf573
garydsong <ceouls@gmail.com>
2022-10-25 10:50:24 -0700

    user route

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,14 @@
-from flask import Blueprint, jsonify
-from flask_login import login_required
-from app.models import User
+from flask import Blueprint, request, jsonify
+from flask_login import current_user, login_required
+from app.models import User, db
+from forms.signup_form import SignUpForm
+
+def validation_form_errors(validation_errors):
+  errors = []
+  for field in validation_errors:
+    for err in validation_errors[field]:
+      errors.append(f'{field}:{err}')
+  return errors
 
 user_routes = Blueprint('users', __name__)
 
@@ -17,3 +25,37 @@ def users():
 def user(id):
     user = User.query.get(id)
     return user.to_dict()
+
+
+## EDIT A USER
+@user_routes.route('/<int:id>', methods=['PUT'])
+@login_required
+def edit_a_user(id):
+  user = User.query.get(id)
+  if not user:
+    return {"message":"User couldn't be found", "statusCode":404}
+
+  if user.owner_id != current_user.id:
+    return {"message":"Forbidden", "statusCode":403}
+
+  form = SignUpForm()
+  form['csrf_token'].data = request.cookies['csrf_token']
+  print('\n\n\n\n\n\n\n form data', form.data)
+  if form.validate_on_submit():
+
+    user.username = form.business_name.data
+    user.email = form.email.data
+    user.first_name = form.first_name.data
+    user.owner_id = current_user.id
+    user.city = form.city.data
+    user.state = form.state.data
+    user.bio = form.bio.data
+    user.cover_img = form.cover_img.data
+    user.website = form.website.data
+
+    db.session.commit()
+
+    updated_user = user.to_dict()
+
+    return updated_user
+  return {"errors": validation_form_errors(form.errors), "statusCode":401}
