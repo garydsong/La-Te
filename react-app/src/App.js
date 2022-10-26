1b0b9bf8c3595d07077f1c0bf6239f9181f5b40a
garydsong <ceouls@gmail.com>
2022-10-25 20:26:25 -0700

    started splash page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,6 +7,7 @@ import NavBar from './components/NavBar';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
+import SplashPage from './components/SplashPage/SplashPage';
 import { authenticate } from './store/session';
 
 function App() {
@@ -28,6 +29,9 @@ function App() {
     <BrowserRouter>
       <NavBar />
       <Switch>
+      <ProtectedRoute path='/' exact={true} >
+          <SplashPage />
+        </ProtectedRoute>
         <Route path='/login' exact={true}>
           <LoginForm />
         </Route>
@@ -40,9 +44,7 @@ function App() {
         <ProtectedRoute path='/users/:userId' exact={true} >
           <User />
         </ProtectedRoute>
-        <ProtectedRoute path='/' exact={true} >
-          <h1>My Home Page</h1>
-        </ProtectedRoute>
+
       </Switch>
     </BrowserRouter>
   );
